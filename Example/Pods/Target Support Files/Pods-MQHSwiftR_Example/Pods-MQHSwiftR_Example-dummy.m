1076a55ae3d3e98f3ca20aa32513450bdac6f87f
Mqhong <617257112@qq.com>
2016-01-19 09:05:42 +0800

    0.1.3

---
+++ Example/Pods/Target Support Files/Pods-MQHSwiftR_Example/Pods-MQHSwiftR_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MQHSwiftR_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MQHSwiftR_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-MQHSwiftR_Tests/Pods-MQHSwiftR_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MQHSwiftR_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MQHSwiftR_TestsVersionString[];
+
